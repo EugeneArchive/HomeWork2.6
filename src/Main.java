2a9a2393302b251a0dddc999299d3e24aa3a98ce
Eugene <89818121788@mail.ru>
2022-07-27 20:03:19 +0300

    HW done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
 public class Main {
+  private static final   List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final  List<Integer> nums2 = new ArrayList<>(List.of(6, 206, 1, 2, 2, 4, 4, 5, 5, 20, 8));
     public static void main(String[] args) {
 
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         System.out.println(task1(nums));
-        List<Integer> nums2 = new ArrayList<>(List.of(6, 206, 1, 2, 2, 4, 4, 5, 5, 20, 8));
+
         System.out.println(task2(nums2));
         String text = "One Six Two Three Four One Three Five Five Six One One";
+        task2b();
         System.out.println(task3(text));
-        System.out.println(task4(text));
-//task4(text);
+      //  System.out.println(task4(text));
+task4(text);
     }
 
     private static String task1(List<Integer> list) {
@@ -24,7 +26,7 @@ public class Main {
     }
 
     private static String task2(List<Integer> list) {
-        Set<Integer> numbers = new TreeSet<>();
+                Set<Integer> numbers = new TreeSet<>();
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) % 2 == 0) {
                 numbers.add(list.get(i));
@@ -34,36 +36,57 @@ public class Main {
     }
 
     private static String task3(String text) {
-        Set<String> textSet = new HashSet<>();
         String[] words = text.split(" ");
+        Set<String> textSet = new HashSet<>();
         for (String word : words) {
             textSet.add(word);
         }
         return textSet.toString();
     }
 
-    private static String task4(String text) {
-        List<String> words = List.of(text.split(" "));
-        Map<Integer, String> differentWords =new HashMap<>();
-        for (int i = 0; i < words.size(); i++) {
-            differentWords.put(i, words.get(i));
-        }
-        for (int i = 0; i < differentWords.size(); i++) {
-            int k =0;
-            if (differentWords.containsValue(words.get(i))) {
-                System.out.println(++k);
+    public static void task2b() {
+        Collections.sort(nums2);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums2) {
+            if ((num % 2) == 0 && !(num == prevNum)) {
+                System.out.println(num);
+                prevNum = num;
             }
         }
-//        List<Integer> differentWords = new ArrayList<>();
-//        Set<String> differentWordsSet = new HashSet<>();
-//        for (int i = 0; i < words.size(); i++) {
-//            differentWordsSet.add(words.get(i));
-//        }
-//        for (int i = 0; i < i; i++) {
-//            differentWordsSet..contains(words.get(i));
-//            System.out.println(i);
+    }
 
+    private static void task4(String text) {
+        String[] words = text.split(" ");
+        Set<String> textSet = new HashSet<>();
+        for (String word : words) {
+            textSet.add(word);
         }
+        System.out.println(words.length - textSet.size());
+
+    }
+
+//    private static String task4(String text) {
+//        List<String> words = List.of(text.split(" "));
+//        Map<Integer, String> differentWords =new HashMap<>();
+//        for (int i = 0; i < words.size(); i++) {
+//            differentWords.put(i, words.get(i));
+//        }
+//        for (int i = 0; i < differentWords.size(); i++) {
+//            int k =0;
+//            if (differentWords.containsValue(words.get(i))) {
+//                System.out.println(++k);
+//            }
+//        }
+////        List<Integer> differentWords = new ArrayList<>();
+////        Set<String> differentWordsSet = new HashSet<>();
+////        for (int i = 0; i < words.size(); i++) {
+////            differentWordsSet.add(words.get(i));
+////        }
+////        for (int i = 0; i < i; i++) {
+////            differentWordsSet..contains(words.get(i));
+////            System.out.println(i);
+//
+//        }
 //        System.out.println(words.toString());
 //        for (int i = 0; i < words.size(); i++) {
 //            int k = 0;
@@ -76,4 +99,3 @@ public class Main {
 //   }
     //   return differentWords.toString();
     }
-}
